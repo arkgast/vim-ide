b864a99b653f3a63af98a2cb10a7c93dbadd697d
Arnold Gandarillas Castillo <arkgast@gmail.com>
2022-08-09 22:12:57 -0400

    feat: update alpha configuration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -66,7 +66,7 @@ require("packer").startup(function(use)
   use({
     "goolord/alpha-nvim",
     config = function()
-      require("alpha").setup(require("alpha.themes.dashboard").config)
+      require("plugins.alpha")
     end,
   })
 
