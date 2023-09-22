294f5b65d00a4990f76a5e0052489a3037de226e
Arnold Gandarillas Castillo <arkgast@gmail.com>
2023-09-22 18:11:27 -0400

    refactor: update lspconfig & remove null-ls config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,14 +1,43 @@
 local lspconfig = require("lspconfig")
 
-local api = vim.api
-local cmd = vim.cmd
-local log = vim.log
-local lsp = vim.lsp
 local map = vim.keymap.set
-local notify = vim.notify
+local api = vim.api
+local lsp = vim.lsp
+
+-- config
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    map('n', 'gD', lsp.buf.declaration, opts)
+    map('n', 'gd', lsp.buf.definition, opts)
+    map("n", "gi", lsp.buf.implementation, opts)
+    map("n", 'gr', vim.lsp.buf.references, opts)
+    map('n', 'gt', vim.lsp.buf.type_definition, opts)
+    map("n", "ga", lsp.buf.code_action)
+    map("n", "<leader>oi", ":OrganizeImports<CR>", opts)
+    map("n", "<leader>rn", lsp.buf.rename, opts)
+    map("n", "<leader>q", lsp.diagnostic.set_loclist, opts)
+    map('n', '<leader>i', vim.lsp.buf.hover, opts)
+    map('n', '<C-i>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<leader>=', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
 
 -- functions
-local function organize_imports()
+-- typescript
+local function ts_imports()
   local params = {
     command = "_typescript.organizeImports",
     arguments = { api.nvim_buf_get_name(0) },
@@ -17,87 +46,43 @@ local function organize_imports()
   lsp.buf.execute_command(params)
 end
 
-local async_formatting = function(bufnr)
-  bufnr = bufnr or api.nvim_get_current_buf()
-
-  lsp.buf_request(bufnr, "textDocument/formatting", lsp.util.make_formatting_params({}), function(err, res, ctx)
-    if err then
-      local err_msg = type(err) == "string" and err or err.message
-      -- you can change the log message / level (or ignore it)
-      notify("formatting: " .. err_msg, log.levels.WARN)
-      return
-    end
-
-    -- don't apply results if buffer is unloaded or has been modified
-    if not api.nvim_buf_is_loaded(bufnr) or api.nvim_buf_get_option(bufnr, "modified") then
-      return
-    end
-
-    if res then
-      local client = lsp.get_client_by_id(ctx.client_id)
-      lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-      api.nvim_buf_call(bufnr, function()
-        cmd("silent noautocmd update")
-      end)
-    end
-  end)
-end
-
-local augroup = api.nvim_create_augroup("LspFormatting", {})
-local function null_ls_on_attach(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
-    api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    api.nvim_create_autocmd("BufWritePost", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        async_formatting(bufnr)
-      end,
-    })
-  end
-end
-
--- setup lsp
-local on_attach = function(client, bufnr)
-  api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  map("n", "gD", lsp.buf.declaration, bufopts)
-  map("n", "gd", lsp.buf.definition, bufopts)
-  map("n", "gi", lsp.buf.implementation, bufopts)
-  map("n", "ga", lsp.buf.code_action)
-  map("n", "<leader>i", lsp.buf.hover, bufopts)
-  map("n", "<leader>=", lsp.buf.formatting, bufopts)
-  map("n", "<leader>rn", lsp.buf.rename, bufopts)
-  map("n", "<leader>oi", ":OrganizeImports<CR>", bufopts)
-
-  null_ls_on_attach(client, bufnr)
-end
-
-local capabilities = lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
+-- Language servers setup
 -- typescript
 lspconfig.tsserver.setup({
-  on_attach = on_attach,
   capabilities = capabilities,
   commands = {
     OrganizeImports = {
-      organize_imports,
-      description = "Organize Imports",
+      ts_imports,
+      description = "Organize TS Imports",
     },
   },
 })
 
--- c#
-lspconfig.csharp_ls.setup({
+-- rust
+lspconfig.rust_analyzer.setup({
+  capabilities = capabilities,
+  settings = {
+    ["rust-analyzer"] = {
+      checkOnSave = {
+        command = "clippy",
+      },
+    },
+  },
+})
+
+-- python
+lspconfig.pyright.setup({
   on_attach = on_attach,
   capabilities = capabilities,
 })
 
 -- solidity
 lspconfig.solc.setup({
-  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- c#
+lspconfig.csharp_ls.setup({
   capabilities = capabilities,
 })
 
@@ -122,23 +107,4 @@ lspconfig.lua_ls.setup({
       },
     },
   },
-})
-
--- rust
-lspconfig.rust_analyzer.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    ["rust-analyzer"] = {
-      checkOnSave = {
-        command = "clippy",
-      },
-    },
-  },
-})
-
--- python
-lspconfig.pyright.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
 })
