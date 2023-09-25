bfb198b0b637406cdbfca4d0e156503d28b21a06
Arnold Gandarillas Castillo <arkgast@gmail.com>
2023-09-25 19:43:15 -0400

    style: use space instead of tab

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,210 +1,207 @@
 require("packer").startup(function(use)
-  -- package manager
-  use("wbthomason/packer.nvim")
+	-- package manager
+	use("wbthomason/packer.nvim")
 
-  -- theme
-  use("gruvbox-community/gruvbox")
+	-- theme
+	use("gruvbox-community/gruvbox")
 
-  use({
-    "nvim-lualine/lualine.nvim",
-    requires = { "kyazdani42/nvim-web-devicons", opt = true },
-    config = function()
-      require("plugins.lualine")
-    end,
-  })
+	use({
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("plugins.lualine")
+		end,
+	})
 
-  -- code completion
-  use({
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("plugins.nvim-lspconfig")
-    end,
-  })
+	-- code completion
+	use({
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.nvim-lspconfig")
+		end,
+	})
 
-  use({
-    "hrsh7th/nvim-cmp",
-    requires = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-      "hrsh7th/cmp-nvim-lsp-document-symbol",
-      "saadparwaiz1/cmp_luasnip",
-      "L3MON4D3/LuaSnip",
-      "rafamadriz/friendly-snippets",
-      "onsails/lspkind.nvim",
-    },
-    config = function()
-      require("plugins.nvim-cmp")
-    end,
-  })
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"hrsh7th/cmp-nvim-lsp-document-symbol",
+			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+			"onsails/lspkind.nvim",
+		},
+		config = function()
+			require("plugins.nvim-cmp")
+		end,
+	})
 
-  -- rust
-  use({
-    "simrat39/rust-tools.nvim",
-    config = function()
-      require("plugins.rust-tools")
-    end,
-  })
+	-- rust
+	use({
+		"simrat39/rust-tools.nvim",
+		config = function()
+			require("plugins.rust-tools")
+		end,
+	})
 
-  use({
-    "saecki/crates.nvim",
-    event = { "BufRead Cargo.toml" },
-    requires = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("plugins.crates")
-    end,
-  })
+	use({
+		"saecki/crates.nvim",
+		event = { "BufRead Cargo.toml" },
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("plugins.crates")
+		end,
+	})
 
-  -- autoformat
-  use({
-    "stevearc/conform.nvim",
-    config = function()
-      require("plugins.conform")
-    end,
-  })
+	-- autoformat
+	use({
+		"stevearc/conform.nvim",
+		config = function()
+			require("plugins.conform")
+		end,
+	})
 
-  -- diagnostics
-  use({
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons"
-    },
-    config = function()
-      require("plugins.trouble")
-    end,
-  })
+	-- diagnostics
 
-  -- emmet
-  use("mattn/emmet-vim")
+	use({
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("plugins.trouble")
+		end,
+	})
 
-  -- copilot
-  use("github/copilot.vim")
+	-- emmet
+	use("mattn/emmet-vim")
 
-  -- treesitter
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = function()
-      require("nvim-treesitter.install").update({ with_sync = true })
-    end,
-    config = function()
-      require("plugins.treesitter")
-    end,
-  })
+	-- copilot
+	use("github/copilot.vim")
 
-  -- fuzzy finder
-  use({
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.x",
-    requires = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("plugins.telescope")
-    end,
-  })
+	-- treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+		config = function()
+			require("plugins.treesitter")
+		end,
+	})
 
-  use({
-    "nvim-telescope/telescope-fzf-native.nvim",
-    run = "make",
-  })
+	-- fuzzy finder
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("plugins.telescope")
+		end,
+	})
 
-  use({
-    "nvim-telescope/telescope-frecency.nvim",
-    requires = { "tami5/sqlite.lua" },
-  })
+	use({
+		"nvim-telescope/telescope-fzf-native.nvim",
+		run = "make",
+	})
 
-  -- nvim-tree
-  use({
-    "kyazdani42/nvim-tree.lua",
-    requires = {
-      "kyazdani42/nvim-web-devicons",
-    },
-    config = function()
-      require("plugins.nvim-tree")
-    end,
-  })
+	use({
+		"nvim-telescope/telescope-frecency.nvim",
+		requires = { "tami5/sqlite.lua" },
+	})
 
-  -- startup screen
-  use({
-    "goolord/alpha-nvim",
-    config = function()
-      require("plugins.alpha")
-    end,
-  })
+	-- nvim-tree
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons",
+		},
+		config = function()
+			require("plugins.nvim-tree")
+		end,
+	})
 
-  -- git
-  use({ "tpope/vim-fugitive" })
+	-- startup screen
+	use({
+		"goolord/alpha-nvim",
+		config = function()
+			require("plugins.alpha")
+		end,
+	})
 
-  use({
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("plugins.gitsigns")
-    end,
-  })
+	-- git
+	use({ "tpope/vim-fugitive" })
 
-  -- utils
-  use({
-    "Pocco81/true-zen.nvim",
-    config = function()
-      require("true-zen").setup()
-    end,
-  })
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("plugins.gitsigns")
+		end,
+	})
 
-  use({
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("plugins.indent-blankline")
-    end,
-  })
+	-- utils
+	use({
+		"Pocco81/true-zen.nvim",
+		config = function()
+			require("true-zen").setup()
+		end,
+	})
 
-  use({
-    "lewis6991/impatient.nvim",
-    config = function()
-      require("impatient").enable_profile()
-    end,
-  })
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("plugins.indent-blankline")
+		end,
+	})
 
-  -- editing
-  use({
-    "numToStr/Comment.nvim",
-    config = function()
-      require("Comment").setup()
-    end,
-  })
+	use({
+		"lewis6991/impatient.nvim",
+		config = function()
+			require("impatient").enable_profile()
+		end,
+	})
 
-  use("simnalamburt/vim-mundo")
+	-- editing
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
 
-  use("chaoren/vim-wordmotion")
+	use("simnalamburt/vim-mundo")
 
-  use({
-    "tpope/vim-surround",
-    requires = {
-      "tpope/vim-repeat",
-    },
-  })
+	use("chaoren/vim-wordmotion")
 
-  use({
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup()
-    end,
-  })
+	use({
+		"tpope/vim-surround",
+		requires = {
+			"tpope/vim-repeat",
+		},
+	})
 
-  use({
-    "Shatur/neovim-session-manager",
-    config = function()
-      require("session_manager").setup({
-        autoload_mode = "Disabled",
-        autosave_last_session = false,
-      })
-    end,
-  })
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	})
 
-  -- tmux
-  use("christoomey/vim-tmux-navigator")
+	use({
+		"Shatur/neovim-session-manager",
+		config = function()
+			require("session_manager").setup({
+				autoload_mode = "Disabled",
+				autosave_last_session = false,
+			})
+		end,
+	})
+
+	-- tmux
+	use("christoomey/vim-tmux-navigator")
 end)
