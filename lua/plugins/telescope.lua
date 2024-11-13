963869b5947d8639fea39322b09db2cc491fe2c5
Arnold Gandarillas Castillo <arkgast@gmail.com>
2024-11-13 17:28:41 -0400

    feat: improve keybindings for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,11 +11,31 @@ telescope.setup({
       i = {
         ["<C-j>"] = actions.move_selection_next,
         ["<C-k>"] = actions.move_selection_previous,
-        ["<Esc>"] = actions.close,
+        ["<Esc>"] = actions.close, -- Keep this to ensure Telescope closes with Esc
+        ["<CR>"] = actions.select_tab,
+        ["<C-u>"] = actions.preview_scrolling_up,
+        ["<C-d>"] = actions.preview_scrolling_down,
+        ["<C-x>"] = actions.select_horizontal, -- split horizontally
+        ["<C-v>"] = actions.select_vertical, -- split vertically
+        ["<C-l>"] = actions.complete_tag, -- complete tag
+        ["<C-/>"] = actions.which_key, -- which key help
       },
     },
     layout_strategy = "vertical",
     layout_config = { width = 0.90, height = 0.90 },
+    sorting_strategy = "ascending",
+    file_ignore_patterns = {
+      "node_modules/*",
+      "target/*",
+      "obj/*",
+      -- "bin/*",
+      "undodir/*",
+    },
+  },
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    },
   },
   extensions = {
     fzf = {
@@ -36,11 +56,12 @@ telescope.setup({
         -- csharp
         "obj/*",
         "bin/*",
+        -- nvim
+        "undodir/*",
       },
       workspaces = {
         ["minka"] = os.getenv("HOME") .. "/Projects/Minka/minka_",
         ["config"] = os.getenv("HOME") .. "/Projects/Vim/vim-ide",
-        ["veloud"] = os.getenv("HOME") .. "/Projects/Veloud/tefia_",
       },
     },
   },
